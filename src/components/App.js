fb328a7eb299169f0b4bcc259fc7f6725a014053
celiarodriguezmo <celiro02@gmail.com>
2022-04-22 11:28:17 +0200

    add function renderSolucionLetters

--- src/components/App.js
+++ src/components/App.js
@@ -9,64 +9,79 @@ import { useState } from "react";
 
 function App() {
   const [numberOfErrors, setNumberOfErrors] = useState(0);
-  const [lastLetter, setLastLetter] = useState('');
+  const [lastLetter, setLastLetter] = useState("");
+  const [word, setWord] = useState("katakroker");
+
   const handleButton = (event) => {
     event.preventDefault();
     setNumberOfErrors(numberOfErrors + 1);
+  };
+  const renderSolutionLetters = () => {
+    const wordLetters = word.split("");
 
+    const html = wordLetters.map((wordLetter, index) => {
+      return <li className='letter' key={index}></li>;
+    });
+
+    return html;
   };
 
   const handleInput = (ev) => {
-    const userLetter = ev.target.value;
-    setLastLetter(userLetter);
-    console.log(lastLetter);
-    // continuar por aquí
-  }
+    const inputValue = ev.target.value;
+    let letters = /^[a-zA-ZÀ-ÿñÑ]$/;
+    if (letters.test(inputValue)) {
+      setLastLetter(inputValue);
+    } else {
+      setLastLetter("");
+    }
+  };
 
   return (
     <div>
-      <div className="page">
+      <div className='page'>
         <header>
-          <h1 className="header__title">Juego del ahorcado</h1>
+          <h1 className='header__title'>Juego del ahorcado</h1>
         </header>
-        <main className="main">
+        <main className='main'>
           <section>
-            <div className="solution">
-              <h2 className="title">Solución:</h2>
-              <ul className="letters">
-                <li className="letter">k</li>
-                <li className="letter">a</li>
-                <li className="letter"></li>
-                <li className="letter">a</li>
-                <li className="letter">k</li>
-                <li className="letter">r</li>
-                <li className="letter"></li>
-                <li className="letter">k</li>
-                <li className="letter">e</li>
-                <li className="letter">r</li>
+            <div className='solution'>
+              <h2 className='title'>Solución: </h2>
+
+              <ul className='letters'>
+                {renderSolutionLetters()}
+                {/* <li className='letter'>k</li>
+                <li className='letter'>a</li>
+                <li className='letter'></li>
+                <li className='letter'>a</li>
+                <li className='letter'>k</li>
+                <li className='letter'>r</li>
+                <li className='letter'></li>
+                <li className='letter'>k</li>
+                <li className='letter'>e</li>
+                <li className='letter'>r</li>*/}
               </ul>
             </div>
-            <div className="error">
-              <h2 className="title">Letras falladas:</h2>
-              <ul className="letters">
-                <li className="letter">f</li>
-                <li className="letter">q</li>
-                <li className="letter">h</li>
-                <li className="letter">p</li>
-                <li className="letter">x</li>
+            <div className='error'>
+              <h2 className='title'>Letras falladas:</h2>
+              <ul className='letters'>
+                <li className='letter'>f</li>
+                <li className='letter'>q</li>
+                <li className='letter'>h</li>
+                <li className='letter'>p</li>
+                <li className='letter'>x</li>
               </ul>
             </div>
-            <form className="form">
-              <label className="title" htmlFor="last-letter">
+            <form className='form'>
+              <label className='title' htmlFor='last-letter'>
                 Escribe una letra:
               </label>
               <input
-                autoComplete="off"
-                className="form__input"
-                maxLength="1"
-                type="text"
-                name="last-letter"
-                id="last-letter"
+                autoComplete='off'
+                className='form__input'
+                maxLength='1'
+                type='text'
+                name='last-letter'
+                id='last-letter'
                 value={lastLetter}
                 onChange={handleInput}
               />
@@ -75,19 +90,19 @@ function App() {
           </section>
 
           <section className={`dummy error-${numberOfErrors}`}>
-            <span className="error-13 eye"></span>
-            <span className="error-12 eye"></span>
-            <span className="error-11 line"></span>
-            <span className="error-10 line"></span>
-            <span className="error-9 line"></span>
-            <span className="error-8 line"></span>
-            <span className="error-7 line"></span>
-            <span className="error-6 head"></span>
-            <span className="error-5 line"></span>
-            <span className="error-4 line"></span>
-            <span className="error-3 line"></span>
-            <span className="error-2 line"></span>
-            <span className="error-1 line"></span>
+            <span className='error-13 eye'></span>
+            <span className='error-12 eye'></span>
+            <span className='error-11 line'></span>
+            <span className='error-10 line'></span>
+            <span className='error-9 line'></span>
+            <span className='error-8 line'></span>
+            <span className='error-7 line'></span>
+            <span className='error-6 head'></span>
+            <span className='error-5 line'></span>
+            <span className='error-4 line'></span>
+            <span className='error-3 line'></span>
+            <span className='error-2 line'></span>
+            <span className='error-1 line'></span>
           </section>
         </main>
       </div>
